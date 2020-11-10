c4934857341d2927c8d66fe752cd1b80eec7526d
JohnTheCoolingFan <ivan8215145640@gmail.com>
2020-11-10 22:09:59 +0300

    Updated is_hidden

--- src/main.rs
+++ src/main.rs
@@ -60,9 +60,18 @@ fn new_mod(mod_name: String, info_json_template: &str, changelog_template: &str)
     println!("Succesfully created project {}", mod_name);
 }
 
+/* Old version
 // Function to filter all files we don't want to add to archive
 fn is_hidden(entry: &DirEntry, zip_file_name: &String) -> bool {
     entry.file_name().to_str().unwrap() == zip_file_name || (entry.file_name().to_str().unwrap() != "." &&  entry.file_name().to_str().unwrap().starts_with("."))
+}*/
+
+// Function to filter all files we don't want to add to archive
+fn is_hidden(entry: &DirEntry, zip_file_name: &String) -> bool {
+    let entry_file_name = entry.file_name().to_str().unwrap();
+    entry_file_name == zip_file_name ||
+        (entry_file_name != "." && entry_file_name.starts_with(".")) ||
+        entry_file_name != "build"
 }
 
 // Build mod. Repurposed from rfmp
